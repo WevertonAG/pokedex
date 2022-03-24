6587314ae1328ab3f204e764fc2a4ec9bb767d13
WevertonAG <weverton552@gmail.com>
2022-03-24 16:37:09 -0300

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fetchPokemon = () =>{
     const getPokemonUrl = id =>`https://pokeapi.co/api/v2/pokemon/${id}`
     const pokemonPromises =[]
     
-    for (let i = 1; i<=897; i++){
+    for (let i = 1; i<=898; i++){
         pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
     }
     Promise.all(pokemonPromises).then(pokemons => {
