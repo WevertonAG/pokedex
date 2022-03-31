9822df3c4a0a87516def1d35182b1b893bab4b69
Weverton Alves Guimarães <62063063+WevertonAG@users.noreply.github.com>
2022-03-31 15:56:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const fetchPokemon = () =>{
         pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
     }
     Promise.all(pokemonPromises).then(pokemons => {
-        let d= 0
+        let d= -1
         const lisPokemons = pokemons.reduce((accumulator, pokemon)=>{
             const types = pokemon.types.map(typeInfo => typeInfo.type.name)
             accumulator += `
